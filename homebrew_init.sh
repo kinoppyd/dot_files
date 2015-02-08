fb2d6ae96c7856381549c866df6adff53444975c
Kinoshita.Yasuhiro <WhoIsDissolvedGirl+github@gmail.com>
2015-02-09 03:16:17 +0900

    change Homebrew install URL

--- homebrew_init.sh
+++ homebrew_init.sh
@@ -3,7 +3,7 @@
 # install homebrew
 cd ~/
 xcode-select --install
-ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew doctor
 
 # brewfile
