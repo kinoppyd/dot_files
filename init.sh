1483f982a5a781b81a41f63e31a1cd53b31fd649
kinoppyd <WhoIsDissolvedGirl+github@gmail.com>
2017-04-17 18:20:09 +0000

    Clone depth 1t

--- init.sh
+++ init.sh
@@ -93,7 +93,7 @@ init_tmux_plugins()
     if [ ! -e $HOME/.tmux ]
     then
       mkdir -p $HOME/.tmux/plugins
-      git clone https://github.com/erikw/tmux-powerline.git $HOME/.tmux/plugins/tmux-powerline || exit 1
+      git clone --depth 1 https://github.com/erikw/tmux-powerline.git $HOME/.tmux/plugins/tmux-powerline || exit 1
       echo 'create tmux plugin dir & clone powerline'
     else
       echo '.tmux/plugins dir already exists'
@@ -110,7 +110,7 @@ clone_git_repository()
   if [ ! -e $HOME/Development/git ]
   then
     echo 'clone git/git repository'
-    git clone https://github.com/git/git $HOME/Development/git
+    git clone --depth 1 https://github.com/git/git $HOME/Development/git
   else
     echo 'git tools already exists'
   fi
