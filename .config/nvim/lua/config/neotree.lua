dff7220dc174ff7fd0ee2f3a4c362eeb50863914
kinoppyd <ppyd@kinoppyd.dev>
2026-01-28 11:52:25 +0900

    Add Neotree config for toggle left

--- .config/nvim/lua/config/neotree.lua
+++ .config/nvim/lua/config/neotree.lua
@@ -1,4 +1,5 @@
 -- Neotree
 vim.api.nvim_set_keymap("n", "<leader>n", ":Neotree float toggle<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>N", ":Neotree left toggle<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>b", ":Neotree buffers float toggle<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>g", ":Neotree git_status float toggle<CR>", { noremap = true, silent = true })
